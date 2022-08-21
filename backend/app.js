4c51f2ec866a533d03b7a4bc479949f567b190db
javier <almizdrano21@hotmail.com>
2022-08-21 12:49:20 +0200

    fix lista

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ const {
   updateAsistente,
   deleteAsistente,
 } = require("./controllers/asistentesControllers");
+
 //CONEXIÓN BASE DE DATOS
 const db = mysql.createConnection({
   host: "hl1132.dinaserver.com",
@@ -21,15 +22,15 @@ const db = mysql.createConnection({
   password: "%4MQ1Ng1[$V2",
   database: "depen_aguasapo",
 });
+
 db.connect(function (err) {
   if (err) {
     return console.error("error: " + err.message);
   }
   console.log("Conectado a la base de datos");
 });
+
 const app = express();
-app.use(cors());
-app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.listen(8000, () => {
@@ -39,6 +40,18 @@ app.listen(8000, () => {
 app.get("/lista", (req, res) => {
   res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
 });
+
+/**
+ * No te esta funcionando porque:
+ * 1. En asistenteModel, tenias puesto que la base de datos es concierto, pero se llama "conciertos"
+ * 2. Las funciones que tienes estas de get, primero que no tenían ningún return ni nada que hiciera que devolvieran algo, solo
+ * tienen console.log
+ * 3. Cuando javascript va leyendo las lineas de código, si una función no tiene lo parentesis "funcion()", no la va a ejecutar.
+ * No hay que confundir una función cuando la ponemos para que se ejecute (llamarla) a cuando la pasamos como parametro a otra funcion,
+ * que ahí lo que le pasamos es una referencia a la misma ===> function() !== funcion.
+ * 4. Lo tienes montado de puta madre, pero te vas a tener que calentar un poco la cabeza para hacer funcionar las funciones estas de
+ * getAsistentes y tal. Si tienes prisa, hazlo como he hecho yo aquí (aunque mi solución sea la facilona cutrona, pa salvar el día puede funcionar)
+ */
 
 app.get("/asistentes", function (req, res) {
   try {
